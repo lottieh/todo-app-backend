9ec024737d2f3b5130762c5c15aabdd679e6e9e2
Charlotte House <cdmhouse@sky.com>
2020-02-09 12:06:28 +0000

    cleaned up code

--- handler.js
+++ handler.js
@@ -15,21 +15,3 @@ app.get('/tasksURL', function (req, res) {
 module.exports.tasks = serverless(app);
 
 //        the above .tasks needs to match our function in our .yml file 
-
-
-// module.exports.tasks = async event => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(
-//       {
-//         message: 'Go Serverless v1.0! Your function executed successfully!',
-//         input: event,
-//       },
-//       null,
-//       2
-//     ),
-//   };
-
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
