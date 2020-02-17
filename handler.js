ac6a3fd772337cd43bf35131b074908b8b5f3b2a
Charlotte House <cdmhouse@sky.com>
2020-02-17 19:25:24 +0000

    add tasks and endpoint holders

--- handler.js
+++ handler.js
@@ -4,16 +4,44 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
-// req - request , res - response
 
+// req - request , res - response
+//retrieve tasks 
 app.get('/tasksURL', function (req, res) {
+  const yourTasks = [
+    { id: 1, description: 'Write CV', Completed: false, important: false },
+    { id: 2, description: 'Learn to Code', Completed: false, important: false },
+    { id: 3, description: 'Find a Flat', Completed: false, important: false },
+    { id: 4, description: 'Test it', Completed: true, important: false }
+
+  ]
   res.json({
-    message: { id:"1", description: 'Write CV', Date: 0, Completed: false, important: false },
-  
-  
-  
-  })
+    tasks: yourTasks
+  });
 });
+
+app.post('/tasksURL', function (req, res) {
+    res.json({
+    
+  });
+});message: 'Your Post works'
+
+app.put('/tasksURL/:taskId', function (req, res) {
+  
+  res.json({
+    message: 'Your Put works'
+  });
+});
+
+app.delete('/tasksURL/:taskId', function (req, res) {
+ 
+  res.json({
+    message: 'Your Delete works'
+  });
+});
+
+
 module.exports.tasks = serverless(app);
 
 //        the above .tasks needs to match our function in our .yml file 
+
