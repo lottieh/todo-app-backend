b81090645b61c1f328349004278c20c2f2a0ec11
Charlotte House <cdmhouse@sky.com>
2020-03-03 22:44:41 +0000

    Update Dump

--- database.sql
+++ database.sql
@@ -23,17 +23,17 @@ DROP TABLE IF EXISTS `tasks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tasks` (
-  `taskId` bigint(20) NOT NULL AUTO_INCREMENT,
   `description` varchar(50) DEFAULT NULL,
   `dueDATE` date DEFAULT NULL,
-  `important` bit(1) DEFAULT NULL,
-  `die` bit(1) DEFAULT NULL,
-  `completed` bit(1) DEFAULT NULL,
   `userid` varchar(50) NOT NULL,
+  `taskId` varchar(255) NOT NULL,
+  `del` tinyint(1) DEFAULT NULL,
+  `important` tinyint(1) DEFAULT NULL,
+  `completed` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`taskId`),
   KEY `fk_id` (`userid`),
   CONSTRAINT `fk_id` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `tasks` (
 
 LOCK TABLES `tasks` WRITE;
 /*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (1,'Write CV','0000-00-00',_binary '',_binary '',_binary '','1'),(2,'Learn to Code','0000-00-00',_binary '',_binary '',_binary '','1'),(3,'Find a Flat','0000-00-00',_binary '',_binary '',_binary '','1'),(4,'Drink Tea','0000-00-00',_binary '',_binary '',_binary '','1');
+INSERT INTO `tasks` VALUES ('Have a working POST API','0000-00-00','1','0de5f68b-93e6-404e-9070-8c25f5b2ef0b',0,0,0),('Have a working GET API','0000-00-00','1','4c5e06f5-58dd-4007-9e65-9cb45d1af707',0,0,0),('Complete Backend API','0000-00-00','1','58bc2f8b-152a-49e5-96db-72aaea06b28e',0,0,0),('Apply for BBC Software Engineering job!','0000-00-00','1','a17515a0-8823-4c3c-9a6a-382b2c92fb40',0,0,0),('Boop Lottie','0000-00-00','1','f0cba0f1-c141-4953-96e8-18f2d8091cfc',0,0,0);
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-24 21:53:10
+-- Dump completed on 2020-03-03 22:44:20
